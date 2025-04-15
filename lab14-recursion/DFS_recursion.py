756ace400921a1f5790719d91ed0f69917eea927
linyueCS-IT <158083329+linyueCS-IT@users.noreply.github.com>
2025-04-15 10:39:07 -0400

    Add DFS-visit logic by recursion

---
+++ lab14-recursion/DFS_recursion.py
@@ -0,0 +1,20 @@
+# A B C
+# D E F
+# G H I
+#
+# start =A
+# goal = I
+# current = A
+# direction = down
+# current = D
+# direction = down
+# current = G
+# direction = right
+# current = H
+# direction = right
+
+# def solve(self) -> Iterable[Direction]:
+#     super().solve()
+#
+#
+# def dfs_visit(self, current: Positon, visited: set(),path)
